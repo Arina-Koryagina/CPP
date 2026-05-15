31a4292f542d0ae0471c3de13fc738273111420d
Arina-Koryagina <ariannakor.2000@gmail.com>
2026-05-15 19:45:17 +0300

    Add project files.

---
+++ CPP/CPP.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<Windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(65001);
+	SetConsoleOutputCP(65001);
+	cout.setf(ios::boolalpha);
+
+
+	//cout << "Hello \"World!\"\n" << endl;
+	//cout << "\t\tMy name is Reggie" << endl;
+	//cout << 125 << endl;
+	//cout << 125.354 << endl;
+	//cout << true << endl;
+
+
+	// type name;
+
+	// bool - 1b
+
+	// char - 1b
+
+
+	return 0;
+}
