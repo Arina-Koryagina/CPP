af52557a4de2fc8028400f05dcf1840a28d041e3
Arina-Koryagina <ariannakor.2000@gmail.com>
2026-06-13 15:33:16 +0300

    29/09/2026

--- Homework_5/Homework_5.cpp
+++ Homework_5/Homework_5.cpp
@@ -11,7 +11,7 @@ int main()
 	srand(time(0));
 
 	/*----------------------*/
-	/*const int size = 11;
+	const int size = 11;
 	int a[size] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
 	for (size_t i = 0; i < size; i++) {
 		cout << a[i] << " ";
@@ -33,7 +33,7 @@ int main()
 	for (size_t i = 0; i < ind - 4 + 3*(size%2); i++) {
 		cout << c[i] << " ";
 	}
-	cout << endl;*/
+	cout << endl;
 	/*----------------------*/
 
 
@@ -46,15 +46,15 @@ int main()
 	// - Вивести список користувачів;
 	// - Вихід.
 
-	/*const int size = 10;
-	int phone[size];
-	int home[size];
+	const int size_1 = 10;
+	int phone[size_1];
+	int home[size_1];
 	int action;
 
-	int min = 503600000, max = 999999999;
-	for (size_t i = 0; i < size; i++) {
-		phone[i] = rand() % (max - min + 1) + min;
-		home[i] = rand() % (max - min + 1) + min;
+	int min_1 = 503600000, max_1 = 999999999;
+	for (size_t i = 0; i < size_1; i++) {
+		phone[i] = rand() % (max_1 - min_1 + 1) + min_1;
+		home[i] = rand() % (max_1 - min_1 + 1) + min_1;
 	}
 
 	bool run = true;
@@ -69,9 +69,9 @@ int main()
 		switch (action)
 		{
 		case 1:
-			for (size_t j = 0; j < size - 1; j++)
+			for (size_t j = 0; j < size_1 - 1; j++)
 			{
-				for (size_t i = 0; i < size - 1 - j; i++)
+				for (size_t i = 0; i < size_1 - 1 - j; i++)
 				{
 					if (phone[i] > phone[i+1])
 					{
@@ -86,15 +86,15 @@ int main()
 			}
 			cout << "Here you go!" << endl;
 			cout << setw(4) << "n. " << "  phone number" << " -  home  number\n" << endl;
-			for (size_t i = 0; i < size; i++) {
+			for (size_t i = 0; i < size_1; i++) {
 				cout << setw(2) << i + 1 << ".  +380" << phone[i] << " - +380" << home[i] << endl;
 			}
 			cout << endl;
 			break;
 		case 2:
-			for (size_t j = 0; j < size - 1; j++)
+			for (size_t j = 0; j < size_1 - 1; j++)
 			{
-				for (size_t i = 0; i < size - 1 - j; i++)
+				for (size_t i = 0; i < size_1 - 1 - j; i++)
 				{
 					if (home[i] > home[i + 1])
 					{
@@ -109,14 +109,14 @@ int main()
 			}
 			cout << "Here you go!" << endl;
 			cout << setw(4) << "n. " << "  phone number" << " -  home  number\n" << endl;
-			for (size_t i = 0; i < size; i++) {
+			for (size_t i = 0; i < size_1; i++) {
 				cout << setw(2) << i + 1 << ".  +380" << phone[i] << " - +380" << home[i] << endl;
 			}
 			cout << endl;
 			break;
 		case 3:
 			cout << setw(4) << "n. " << "  phone number" << " -  home  number\n" << endl;
-			for (size_t i = 0; i < size; i++) {
+			for (size_t i = 0; i < size_1; i++) {
 				cout << setw(2) << i + 1 << ".  +380" << phone[i] << " - +380" << home[i] << endl;
 			}
 			cout << endl;
@@ -129,7 +129,7 @@ int main()
 			cout << "Please, use 1-4 only!" << endl;
 			break;
 		}
-	} while (run);*/
+	} while (run);
 
 
 	//  II
@@ -138,7 +138,51 @@ int main()
 	// Удосконалення полягає в тому, щоб аналізувати кількість перестановок на кожному кроці, якщо ця
 	// кількість дорівнює нулю, то продовжувати сортування немає сенсу — масив відсортовано. 
 
+	const int size_2 = 10;
+	int b[size_2];
+	int min_2 = 0, max_2 = 30;
+	for (size_t i = 0; i < size_2; i++)
+	{
+		b[i] = rand() % (max_2 - min_2 + 1) + min_2;
+	}
 
+	for (int i = 0; i < size_2; i++)
+	{
+		cout << b[i] << " ";
+	}
+	cout << endl;
+	cout << endl;
+
+	for (int i = 0; i < size_2 - 1; i++)
+	{
+		int swaps = 0;
+		for (int j = 0; j < size_2 - i - 1; j++)
+		{
+			if (b[j] > b[j + 1])
+			{
+				swap(b[j], b[j + 1]);
+				swaps++;
+			}
+		}
+
+		if (swaps == 0)
+		{
+			break;
+		}
+
+		for (int k = 0; k < size_2; k++)
+		{
+			cout << b[k] << " ";
+		}
+		cout << endl;
+	}
+
+	cout << "Sorted!" << endl;
+	for (int i = 0; i < size_2; i++)
+	{
+		cout << b[i] << " ";
+	}
+	cout << endl;
 
 
 	//  III
@@ -147,24 +191,79 @@ int main()
 	// між будь-якими двома вставляємо лопатку і змінюємо порядок оладок над лопаткою на зворотний.
 	// Необхідно за мінімальну кількість таких операцій відсортувати знизу вгору за зменшенням радіуса.
 
-	const int size = 7;
-	int d[size];
+	const int size_3 = 7;
+	int d[size_3];
 	int min = 3, max = 23;
-	for (size_t i = 0; i < size; i++) {
+	for (size_t i = 0; i < size_3; i++)
+	{
 		d[i] = rand() % (max - min + 1) + min;
 	}
-	for (size_t i = 0; i < size; i++) {
+	for (size_t i = 0; i < size_3; i++)
+	{
 		cout << d[i] << " ";
 	}
 	cout << endl;
+	cout << endl;
 
-	for (size_t i = 0; i < size/2; i++)
+	int num = 1;
+	for (int index = 0; index < size_3 - 1; index++)
 	{
-		int a = d[size - 1 - i];
-		d[size - 1 - i] = d[i];
-		d[i] = a;
+		int maxNum = d[index];
+		int maxIndex = index;
+		for (int i = index; i < size_3; i++)
+		{
+			if (d[i] > maxNum)
+			{
+				maxNum = d[i];
+				maxIndex = i;
+			}
+		}
+
+		if (maxIndex == index)
+		{
+			continue;
+		}
+
+		if (maxIndex != size_3 - 1)
+		{
+			int left = maxIndex;
+			int right = size_3 - 1;
+			while (left < right)
+			{
+				swap(d[left], d[right]);
+				left++;
+				right--;
+			}
+			cout << num << ". ";
+			for (int i = 0; i < size_3; i++)
+			{
+				cout << d[i] << " ";
+			}
+			cout << endl;
+			num++;
+		}
+
+		int left = index;
+		int right = size_3 - 1;
+		while (left < right)
+		{
+			swap(d[left], d[right]);
+			left++;
+			right--;
+		}
+
+		cout << num << ". ";
+		for (int i = 0; i < size_3; i++)
+		{
+			cout << d[i] << " ";
+		}
+		cout << endl;
+		num++;
 	}
-	for (size_t i = 0; i < size; i++) {
+
+	cout << "Sorted!" << endl;
+	for (size_t i = 0; i < size_3; i++)
+	{
 		cout << d[i] << " ";
 	}
 	cout << endl;
