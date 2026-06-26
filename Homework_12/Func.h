972d088b7274e23f2c0551a01576491b29dd984b
Arina-Koryagina <ariannakor.2000@gmail.com>
2026-06-26 13:15:48 +0300

    24/06/2026

--- Homework_12/Func.h
+++ Homework_12/Func.h
@@ -36,6 +36,15 @@ void createArray(T**& arr, int row, int col)
 }
 
 template<class T>
+void setArray(T* arr, int size, int minValue = 0, int maxValue = 9)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		arr[i] = rand() % (maxValue - minValue + 1) + minValue;
+	}
+}
+
+template<class T>
 void setArray(T** arr, int row, int col, int minValue = 0, int maxValue = 9)
 {
 	for (size_t i = 0; i < row; i++)
@@ -76,4 +85,131 @@ void deleteArray(T**& arr)
 	}
 	delete[] arr;
 	arr = nullptr;
+}
+
+template<class T>
+void addColArray(T**& arr, int row, int& col, int ind, T* newCol = nullptr)
+{
+	T** temp = nullptr;
+	createArray(temp, row, col + 1);
+	for (size_t i = 0; i < row; i++)
+	{
+		for (size_t j = 0; j < ind; j++)
+		{
+			temp[i][j] = arr[i][j];
+		}
+		temp[i][ind] = (newCol != nullptr) ? newCol[i] : 0;
+		for (size_t j = ind; j < col; j++)
+		{
+			temp[i][j + 1] = arr[i][j];
+		}
+	}
+	delete[] arr;
+	col++;
+	arr = temp;
+}
+
+template<class T>
+void delColArray(T**& arr, int row, int& col, int ind)
+{
+	T** temp = nullptr;
+	createArray(temp, row, col - 1);
+	for (size_t i = 0; i < row; i++)
+	{
+		for (size_t j = 0; j < ind; j++)
+		{
+			temp[i][j] = arr[i][j];
+		}
+		for (size_t j = ind; j < col; j++)
+		{
+			temp[i][j] = arr[i][j + 1];
+		}
+	}
+	delete[] arr;
+	col--;
+	arr = temp;
+}
+
+template<class T>
+void leftShift(T** a, T** arr, int row, int col, int shift)
+{
+	for (size_t j = 0; j < row; j++)
+	{
+		for (size_t i = 0; i < col; i++)
+		{
+			a[j][i] = arr[j][(i + shift) % col];
+		}
+	}
+}
+
+template<class T>
+void rightShift(T** a, T** arr, int row, int col, int shift)
+{
+	for (size_t j = 0; j < row; j++)
+	{
+		for (size_t i = 0; i < col; i++)
+		{
+			a[j][i] = arr[j][(i - shift + col) % col];
+		}
+	}
+}
+
+template<class T>
+void upShift(T** a, T** arr, int row, int col, int shift)
+{
+	for (size_t j = 0; j < row; j++)
+	{
+		for (size_t i = 0; i < col; i++)
+		{
+			a[j][i] = arr[(j + shift) % row][i];
+		}
+	}
+}
+
+template<class T>
+void downShift(T** a, T** arr, int row, int col, int shift)
+{
+	for (size_t j = 0; j < row; j++)
+	{
+		for (size_t i = 0; i < col; i++)
+		{
+			a[j][i] = arr[(j - shift + row) % row][i];
+		}
+	}
+}
+
+template<class T>
+void shiftArray(T**& arr, int m, int n, int shift, void(*oper)(T**, T**, int, int, int))
+{
+	T** a = nullptr;
+	createArray(a, m, n);
+	oper(a, arr, m, n, shift);
+	deleteArray(arr);
+	arr = a;
+}
+
+template<class T>
+void shiftRowArray(T**& arr, int m, int n)
+{
+	void(*direction[])(T**, T**, int, int, int) = { leftShift, rightShift, upShift, downShift };
+	int dir, shift;
+	cout << "\n1 - up \n2 - down" << endl;
+	cout << "Choose row shift direction: ";
+	cin >> dir;
+	cout << "Choose row shift length: ";
+	cin >> shift;
+	shiftArray(arr, m, n, shift, direction[dir + 1]);
+}
+
+template<class T>
+void shiftColArray(T**& arr, int m, int n)
+{
+	void(*direction[])(T**, T**, int, int, int) = { leftShift, rightShift, upShift, downShift };
+	int dir, shift;
+	cout << "\n1 - left \n2 - right" << endl;
+	cout << "Choose col shift direction: ";
+	cin >> dir;
+	cout << "Choose col shift length: ";
+	cin >> shift;
+	shiftArray(arr, m, n, shift, direction[dir - 1]);
 }
--- Homework_12/Homework_12.cpp
+++ Homework_12/Homework_12.cpp
@@ -19,23 +19,61 @@ int main()
 	cout << "\tI" << endl;
 	// Написати функцію, що додає стовпчик двовимірному масиву в зазначену позицію.
 
+	int row = 3, col = 4;
+	int** p = nullptr;
+	createArray(p, row, col);
+	setArray(p, row, col);
+	printArray(p);
 
+	int* t = new int[row];
+	setArray(t, row);
+	addColArray(p, row, col, 2, t);
+	printArray(p);
 
 
 	//  II
-	cout << "\n\tII" << endl;
+	cout << "\tII" << endl;
 	// Написати функцію, що видаляє стовпчик двовимірного масиву за вказаним номером.
 
-
+	printArray(p);
+	delColArray(p, row, col, 3);
+	printArray(p);
+	deleteArray(p);
 
 
 	//  III
-	cout << "\n\tIII" << endl;
+	cout << "\tIII" << endl;
 	// Дано матрицю порядку MxN (M рядків, N стовпців). Необхідно заповнити її значеннями
 	// і написати функцію, що здійснює циклічний зсув рядків та/або стовпчиків масиву
 	// вказану кількість разів і в зазначену сторону.
 
+	int M = 3, N = 4;
+	int** arr = nullptr;
+	createArray(arr, M, N);
+	setArray(arr, M, N);
+	printArray(arr);
 
+	int choice;
+	cout << "0 - row\n1 - col\n2 - both" << endl;
+	cout << "Choose the shift: ";
+	cin >> choice;
+
+	switch (choice)
+	{
+	case 0:
+		shiftRowArray(arr, M, N);
+		break;
+	case 1:
+		shiftColArray(arr, M, N);
+		break;
+	case 2:
+		shiftRowArray(arr, M, N);
+		shiftColArray(arr, M, N);
+		break;
+	default:
+		break;
+	}
+	printArray(arr);
 
 
 	return 0;
